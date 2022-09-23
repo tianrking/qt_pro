ebd6c6c419bbf9f926b544c8973011ae61675ee5
Tienjui Wang <tian.r.king@gmail.com>
2022-09-23 12:32:03 +0800

    Change to QT6

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,8 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <QMainWindow>
-#include <QSerialPort>
 #include <QSerialPortInfo>
 #include <QNetworkRequest>
 #include <QNetworkReply>
@@ -10,7 +8,19 @@
 
 #include <QMediaPlayer>
 #include <QVideoWidget>
-#include <QMediaPlaylist>
+//#include <QMediaPlaylist>
+
+#include <QMainWindow>                 //主文件头文件
+#include <QSerialPort>                 //串口头文件
+#include <QScatterSeries>              //画点状曲线头文件
+#include <QtCharts/QLineSeries>        //画线曲线头文件
+#include <QtCharts/QValueAxis>         //画坐标轴头文件
+#include <QtCharts/QChartView>         //画绘图区域头文件
+#include <QtCore/QTimer>               //时间定时器头文件
+#include <QtWidgets/QGesture>          //手势头文件（鼠标动作）
+#include <QtWidgets/QGraphicsScene>    //绘图屏幕参数头文件
+#include <QtWidgets/QGraphicsView>     //绘图区域显示头文件
+#include <QtGui/QMouseEvent>           //鼠标动作时间头文件（产生槽函数）
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -45,12 +55,26 @@ private slots:
     void setVideo();
 
 private:
+
     Ui::MainWindow *ui;
 
     QSerialPort *serialPort;
 
     QMediaPlayer *player;
     QVideoWidget *videoWidget;
+
+    //绘图定义变量 Start
+    QChartView *chartView_Vs;
+    QChart *chart_Vs;
+    QLineSeries *m_series_line_Vsua;
+    QLineSeries *m_series_line_Vsub;
+    QLineSeries *m_series_line_Vsuc;
+    QScatterSeries *m_series_point_Vsua;
+    QScatterSeries *m_series_point_Vsub;
+    QScatterSeries *m_series_point_Vsuc;
+    QValueAxis *m_axisX_Vs;
+    QValueAxis *m_axisY_Vs;
+    //绘图定义变量 End
 
 };
 #endif // MAINWINDOW_H
